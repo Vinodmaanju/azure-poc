51ad729c4a4e768f2163c54156715ac04b8d531f
Vinodmaanju <vinodmaanju@gmail.com>
2025-06-13 00:19:33 +0100

    Updated path

--- bronze_notebook.py
+++ bronze_notebook.py
@@ -1,4 +1,4 @@
 # Bronze Layer - Ingest JSON from ADLS
 from pyspark.sql.functions import col, to_date, lower
-df_bronze = spark.read.parquet("abfss://folder_name@<container_name>.dfs.core.windows.net/bronze/Vinodmaanju/azure-poc/refs/heads/main/retail_transactions_bronze.parquet")
+df_bronze = spark.read.parquet("abfss://<container_name>@<storage_account_name>.dfs.core.windows.net/bronze/Vinodmaanju/azure-poc/refs/heads/main/retail_transactions_bronze.parquet")
 df_bronze.show()
